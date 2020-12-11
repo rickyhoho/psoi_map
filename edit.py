1ff71bfa91bc29cfe361fa05743d54efee67711b
HKY_Ricky <836650357@qq.com>
2020-12-12 02:24:54 +0800

    Update edit.py

--- edit.py
+++ edit.py
@@ -10,6 +10,7 @@ filecode = file + "/code"
 dir_remove = file + "/removed"
 dir_remove_code = dir_remove + "/code"
 dir_remove_map = dir_remove + "/map"
+star = False #important
 
 
 if not os.path.exists(filemap) or not os.path.exists(filecode):
@@ -27,13 +28,26 @@ else:
 
 
 def process_ans(input_num, filenum):
-    
+    if star == True:
+        with open(file + "/question.txt", "r") as f1:
+            lines2 = f1.read().splitlines()
+            print(lines2)
+
+        with open(dir_remove + '/question.txt', "a") as f2:
+            f2.write(lines2[input_num - 1] + "\n")
+
+        with open(file + "/question.txt", "w") as f1:
+            for i in range(filenum + 1):
+                if i == input_num - 1:
+                    continue
+                f1.write(lines2[i] + "\n")
+
     with open(file + "/ans.txt", "r") as f1:
         lines = f1.read().splitlines()
         print(lines)
 
-        with open(dir_remove + '/ans.txt', "a") as f2:
-            f2.write(lines[input_num] + "\n")
+    with open(dir_remove + '/ans.txt', "a") as f2:
+        f2.write(lines[input_num] + "\n")
 
     with open(file + "/ans.txt", "w") as f1:
         for i in range(filenum + 1):
@@ -51,13 +65,13 @@ def process(input_num):
     shutil.move(filecode + png_path, dir_remove_code + png_path)
 
     process_ans(input_num, filenum)
-    '''for i in range(input_num, filenum):
+    for i in range(input_num, filenum):
         png_path = "/" + "%05d" % i + ".png"
         png_path1 = "/" + "%05d" % (i + 1) + ".png"
 
         os.rename(filemap + png_path1, filemap + png_path)
         os.rename(filecode + png_path1, filecode + png_path)#old->new
-'''
+
 
 def multi_input():
     list1 = []
@@ -85,6 +99,7 @@ def multi_input():
             list1.sort()
             for i in range(end - 1, start - 1, -1):
                 process(list1[i])
+            input()
             return 0
                 
         elif inp.startswith("show"):
@@ -114,6 +129,22 @@ def single_mode():
 
 
 multi_input()
+
+
+with open(file + "/ans.txt", "r") as f1:
+    lines = f1.read().splitlines()
+
+with open(file + "/ans.txt", "w") as f1:
+        for i in range(len(lines) - 1, -1, -1):
+            f1.write(lines[i] + "\n")
+            
+if star == True:
+    with open(file + "/question.txt", "r") as f1:
+        lines = f1.read().splitlines()
+
+    with open(file + "/question.txt", "w") as f1:
+            for i in range(len(lines) - 1, -1, -1):
+                f1.write(lines[i] + "\n")
 
 
 print("finish")
